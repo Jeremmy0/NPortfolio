7006c0dc13ee38ad4a3e4803ab7656fad8b9ccd6
Ayoola Jeremiah <ayoolajeremy@gmail.com>
2025-07-03 04:30:05 +0100

    Updated the revert

--- src/components/cta.jsx
+++ src/components/cta.jsx
@@ -1,21 +1,16 @@
 
 import { FaArrowRight } from "react-icons/fa"
-import * as FaIcons from "react-icons/fa";
 
-const Cta = ({text , className,href,FaIconName}) => {
-  console.log(FaIconName)
-  const IconComponent = FaIcons[FaIconName] 
-  console.log(FaIcons[FaIconName], IconComponent)
-  
+const Cta = ({text , className,href}) => {
   return (
     // <div className="">
     //     <button className="py-2 px-4 rounded-md content-center text-center bg-white text-black/80 text-lg font-semibold">{text}</button>
     // </div>
-     <button type="submit" className={`py-2 px-3     group rounded-md content-center text-center   text-lg font-semibold ${className} bg-[#008000] text-white hover:bg-[#00a000] transition-all duration-500 ease-out  z-90 `}>
+     <button type="submit" className={`py-2 px-3    hidden md:flex group rounded-md content-center text-center   text-lg font-semibold ${className} bg-[#008000] text-white hover:bg-[#00a000] transition-all duration-500 ease-out  z-90 `}>
       <a href={`${href}`} className="group">{text}</a> 
-      <div className="flex gap-4 relative"> <IconComponent className="self-center place-self-center opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-2 transition-all  duration-500 ease-out absolute "/>
+      <div className="flex gap-4 relative"> <FaArrowRight className="self-center place-self-center opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-2 transition-all  duration-500 ease-out absolute "/>
 
-     <IconComponent className="self-center place-self-center ps-1  group-hover:opacity-100 group-hover:translate-x-2 transition-all  duration-500 ease-out "/> </div></button>
+     <FaArrowRight className="self-center place-self-center ps-1  group-hover:opacity-100 group-hover:translate-x-2 transition-all  duration-500 ease-out "/> </div></button>
   )
 }
 
