76b49f973be746462c6e074323bec123a320c3f6
Ayoola Jeremiah <ayoolajeremy@gmail.com>
2025-07-08 11:29:24 +0100

    update

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { IconsManifest } from "react-icons";
 import * as FaIcons from "react-icons/fa";
+import Link from "next/link";
 
 const NavLink = ({ text, faIconName, href, MobileOpen }) => {
 
@@ -14,21 +15,23 @@ const NavLink = ({ text, faIconName, href, MobileOpen }) => {
       // i am uding this two "" because i know that thw ixon component will return true: incase of error you can try and check if they both dont have bugs
       ? (
         <> 
-          <a href={`${href}`} className="  gap-2 items-baseline inline-flex">
+          <Link href={`${href}`} className="  gap-2 items-baseline inline-flex">
              <IconComponent className="scale-150 group-hover:scale-200 transition-transform duration-300 ease-out opacity-100" />
             <div className=" font-semibold text-xl text-[var(--foregound)] dark:text-[var(--foreground)] ">
-                <span href={`${href}`}> {text} </span> 
+                <span > {text} </span> 
             </div>
-          </a>
+          </Link>
         </>
       ) 
       :(
-        <> 
+        <Link href={`${href}`}>
           <IconComponent className="scale-150 group-hover:scale-200 transition-transform duration-300 ease-out opacity-100" />
          <div className="absolute top-13 font-semibold left-0 opacity-0 z-20 text-[var(--foregound)] dark:text-[var(--foreground)] transition-opacity duration-300 ease-out group-hover:opacity-100">
-            <a href={`${href}`}> {text} </a> 
+            <span > {text} </span> 
          </div>
-      </>
+        </Link> 
+
+
       )}
 
     </div>
